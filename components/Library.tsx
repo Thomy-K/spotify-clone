59016907527c47b05ed5b36c39d65987e12f2032
Thomas Krahwinkel <tkrahwinkel@uc.cl>
2024-01-05 23:17:26 -0300

    songs upload

--- components/Library.tsx
+++ components/Library.tsx
@@ -4,9 +4,9 @@ import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
 // import { Song } from "@/types";
-// import useUploadModal from "@/hooks/useUploadModal";
-// import { useUser } from "@/hooks/useUser";
-// import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+import useAuthModal from "@/hooks/useAuthModal";
 // import useSubscribeModal from "@/hooks/useSubscribeModal";
 // import useOnPlay from "@/hooks/useOnPlay";
 
@@ -16,27 +16,26 @@ import { AiOutlinePlus } from "react-icons/ai";
 //   songs: Song[];
 // }
 
-// const Library: React.FC<LibraryProps> = ({
+const Library = ({
 //   songs
-// }) => {
-//   const { user, subscription } = useUser();
-//   const uploadModal = useUploadModal();
-//   const authModal = useAuthModal();
+}) => {
+  const { user, subscription } = useUser();
+  const uploadModal = useUploadModal();
+  const authModal = useAuthModal();
 //   const subscribeModal = useSubscribeModal();
 
 //   const onPlay = useOnPlay(songs);
 
-const Library = () => {
   const onClick = () => {
-    // if (!user) {
-    //   return authModal.onOpen();
-    // }
+    if (!user) {
+      return authModal.onOpen();
+    }
 
     // if (!subscription) {
     //   return subscribeModal.onOpen();
     // }
 
-    // return uploadModal.onOpen();
+    return uploadModal.onOpen();
   }
 
   return ( 
